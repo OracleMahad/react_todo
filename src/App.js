5806aa315664fd126df017fa6950b29a1f73a7da
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-08-23 04:15:02 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
 
-  id = 3
+  id = 1
 
   state = {
     input: '',
     todos: [
-      { id : 0, text: '리액트 소개', checked: false },
-      { id : 1, text: '리액트 소개', checked: true },
-      { id : 2, text: '리액트 소개', checked: false }
+      { id : 0, text: '할 일', checked: false },
     ]
   }
 
@@ -22,15 +20,70 @@ class App extends Component {
     })
   }
 
-  handleKey
+  handleCreate = () => {
+    const {input, todos} = this.state;
+    this.setState({
+      input: '',
+      todos : todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    if(e.key ==='Enter'){
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    const index = todos.findIndex(todo => todo.id===id);
+    const selected = todos[index];
+
+    const nextTodos = [...todos];
+
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
+  handleRemove = (id) => {
+    const { todos } =this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
 
   render() {
+    const { input, todos} = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this;
+
     return (
-      <div className="App">
-        <TodoListTemplate form={<Form/>}>
-          <TodoItemList/>
+        <TodoListTemplate form={(
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        )}>
+          <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
         </TodoListTemplate>
-      </div>
     );
   }
 }
